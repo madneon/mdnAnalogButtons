f53ef8d511d1f282949829ce197a34a980074597
madneon <bartek.bobko@gmail.com>
2017-04-03 21:47:11 +0200

    Initial import

---
+++ src/mdnAnalogButtons.cpp
@@ -0,0 +1,66 @@
+/*
+	MIT License
+
+	Copyright © 2017 by madneon
+	http://madneon.com
+*/
+
+#include <mdnAnalogButtons.h>
+
+const byte _button_val_none = 255; // full 8 bits
+
+// constructor
+mdnAnalogButtons::mdnAnalogButtons(byte pin, byte count, byte values[]) {
+	_pin = pin;
+	_count = count + 1; // 0 and values
+
+	// alloc memory
+	delete []_values;
+	_values = new byte[count];
+
+	// assign no button value
+	_values[0] = _button_val_none;
+
+	// assign all button values
+	for (size_t i = 0; i < _count; i ++){
+		_values[i + 1] = values[i];
+	}
+}
+
+byte mdnAnalogButtons::pop() {
+	byte _b = _match_pressed();
+
+	if (_pressed_button != _b) {
+		// new button pressed or released
+		_pressed_button = _b;
+		if (_pressed_button != 0) {
+			// its not release
+			_pressed_tick = millis();
+			_pressed_next = _pressed_tick + time_delay;
+		}
+	} else {
+		// same button
+		if (millis() >= _pressed_next) {
+			_pressed_next += time_repeat;
+		} else {
+			return 0; // don't report repeat yet
+		}
+	}
+
+	return _pressed_button;
+}
+
+byte mdnAnalogButtons::_match_pressed() {
+	// read value
+	int _v = analogRead(_pin) >> 2; // 8 highest bits
+
+	for (size_t i = 0; i < _count; i ++){
+		if (abs(_v - _values[i]) <= _spread) { // match within spread range
+			// return matching button id
+			return i;
+		}
+	}
+
+	// no matching value found
+	return 0;
+}
---
+++ src/mdnAnalogButtons.h
@@ -0,0 +1,35 @@
+/*
+	MIT License
+
+	Copyright © 2017 by madneon
+	http://madneon.com
+*/
+
+#ifndef mdnAnalogButtons_h
+#define mdnAnalogButtons_h
+
+#include <Arduino.h>
+
+class mdnAnalogButtons {
+	public:
+		mdnAnalogButtons(byte pin, byte count, byte values[]); // constructor
+
+		unsigned long time_delay = 250; // button repeat delay
+		unsigned long time_repeat = 50; // button repeat after delay
+
+		byte pop();
+	private:
+		byte _pin = 0; // analog pin to read
+
+		byte _count = 0; // number of buttons
+		byte *_values = 0; // values of buttons, 8 highest bits of analogRead(), min. distance = spread*2!
+		byte _spread = 1; // +/- on matching
+
+		byte _pressed_button = 0; // currently pressed button
+		unsigned long _pressed_tick = 0; // tick of first press
+		unsigned long _pressed_next = 0; // tick of next repeated press
+
+		byte _match_pressed();
+};
+
+#endif
